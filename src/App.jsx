42b12322e098c2275431786403d9731d87927d62
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-04-23 16:26:21 -0500

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Link, Outlet } from "react-router-dom"
+import NavMenu from "./components/NavMenu"
 import Menu from "./components/Menu/Index"
 
 export default function App() {
     return (
         <>         
-            <Menu>
-                <Menu.Button>Menu</Menu.Button>
-                <Menu.Dropdown>
-                    <Menu.Item><Link to="recipes">Recipes</Link></Menu.Item>
-                    <Menu.Item><Link to="add-recipe">Add recipe</Link></Menu.Item>
-                    <Menu.Item><Link to="/">Shopping list</Link></Menu.Item>
-                </Menu.Dropdown>
-            </Menu>
+            <NavMenu />
             <Outlet />
         </>
     )
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+import Menu from "./Menu/Index"
+
 export default function NavMenu() {
 
     return (
-        
+        <Menu>
+            <Menu.Button>Menu</Menu.Button>
+            <Menu.Dropdown>
+                <Menu.Item><Link to="recipes">Recipes</Link></Menu.Item>
+                <Menu.Item><Link to="add-recipe">Add recipe</Link></Menu.Item>
+                <Menu.Item><Link to="/">Shopping list</Link></Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
     )
 }
