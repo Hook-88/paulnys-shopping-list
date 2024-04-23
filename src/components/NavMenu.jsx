c0601935e8e9258f619b520895648fad9b7ea9ec
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-04-23 16:36:08 -0500

    add basic styles

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom"
+import { FaBars, FaXmark } from "react-icons/fa6"
 import Menu from "./Menu/Index"
 
 export default function NavMenu() {
 
     return (
         <Menu>
-            <Menu.Button>Menu</Menu.Button>
-            <Menu.Dropdown>
+            <Menu.Button className="text-xl p-3 z-10 relative">
+                <FaBars />
+            </Menu.Button>
+            <Menu.Dropdown
+                className="bg-sky-700 text-white text-xl fixed inset-x-0 top-0 text-center flex flex-col gap-2 py-2 pb-3"
+            >
                 <Menu.Item><Link to="recipes">Recipes</Link></Menu.Item>
                 <Menu.Item><Link to="add-recipe">Add recipe</Link></Menu.Item>
                 <Menu.Item><Link to="/">Shopping list</Link></Menu.Item>
