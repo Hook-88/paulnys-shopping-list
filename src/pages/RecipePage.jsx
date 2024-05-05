7b8803f4ec8d72fc01d6be57ce258c6563e734e2
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-05 11:15:38 -0500

    add basic markup and styles

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from "react"
 import PageHeader from "../components/PageHeader"
+import PageMain from "../components/PageMain"
 import { useParams } from "react-router-dom"
+import { db } from "../firebase"
+import { doc, getDoc } from "firebase/firestore"
+import { IoCheckmarkCircle, IoEllipseOutline } from "react-icons/io5";
+import getCapString from "../utility/getCapedString"
+import List from "../components/List/List"
+import ListItem from "../components/List/ListItem"
+import Button from "../components/Button"
+import PageLink from "../components/PageLink"
+import { FaAngleRight, FaPlus } from "react-icons/fa6"
 
 export default function RecipePage() {
     const { id } = useParams()
-    
-    console.log(id)
+    const [recipe, setRecipe] = useState(null)
+
+    async function getRecipe() {
+        const docRef = doc(db, "recipes", id)
+        const docSnap = await getDoc(docRef)
+
+        setRecipe(docSnap.data())
+    }
+
+    useEffect(() => {
+        getRecipe()
+    }, [])
 
     return (
+        recipe ?
         <div>
             <PageHeader>
-                <h1 className="col-start-2 col-span-4 justify-self-center">Name Recipe</h1>
+                <h1 className="col-start-2 col-span-4 justify-self-center">{getCapString(recipe.name)}</h1>
                 {/* {
                     showAddItem || shoppingList?.items.length > 0 ?
                     <button 
@@ -20,6 +42,57 @@ export default function RecipePage() {
                     </button> : null
                 } */}
             </PageHeader>
-        </div>
+            <PageMain>
+                <List>
+                    {
+                        recipe.ingredients.map((ingredient, index, arr) => {
+                            let classNameGen = "flex items-center justify-between" 
+        
+                            if (index !== arr.length - 1) {
+                                classNameGen += " shadow-[rgba(100,100,100,0.5)_0px_1px_0px_0px]"
+                            }
+        
+                            // if (ingredient.checked) {
+                            //     classNameGen += " flex items-center justify-between text-white/20 line-through italic"
+                            // }
+        
+                            return (
+                                <ListItem
+                                    key={ingredient.id}
+                                    className={classNameGen}
+                                    // onClick={() => togglePropInFirebase(AddItemObj, item.id, "checked")}
+                                >
+                                    {getCapString(ingredient.name)}
+                                    {
+                                        ingredient.checked ?
+                                        <IoCheckmarkCircle className="text-xl text-sky-700"/> :
+                                        <IoEllipseOutline className="text-xl"/>
+                                    }
+                                    {ingredient.checked ? <FaCheck /> : null}
+                                </ListItem>
+                            )
+        
+                        })
+                    }
+                </List>
+
+                <Button className="flex items-center justify-between">
+                    Select all
+                    <IoCheckmarkCircle className="text-xl text-sky-700"/>
+                </Button>
+                
+                <Button className="flex items-center justify-between">
+                    Add to shopping list
+                    <FaPlus />
+                </Button>
+                
+                <PageLink to="/recipes">
+                    Recipes
+                    <FaAngleRight />
+                </PageLink>
+
+
+            </PageMain> 
+        </div> : null
     )
 }
