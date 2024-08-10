8bf342d8c760a230ec83dbbb36bdd9ab9ae634a3
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-08-10 16:39:10 -0500

    add basic route

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const shoppingList = [
+    { id: 1, name: "Milk", quantity: 2, selected: false },
+    { id: 2, name: "Bread", quantity: 1, selected: true },
+    { id: 3, name: "Eggs", quantity: 12, selected: false },
+    { id: 4, name: "Apples", quantity: 6, selected: true },
+    { id: 5, name: "Cheese", quantity: 1, selected: false }
+  ]
---
+++ src/pages/shopping_list/PageShoppingList.tsx
@@ -0,0 +1,6 @@
+export default function PageShoppingList(){
+    
+    return (
+        <h1>This is the shoppping list page</h1>
+    )
+}
