21364c9fb4ff03b6a7814443f71582fd8f6c8938
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-08-12 09:53:26 -0500

    add state

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { createContext, useState } from "react"
 import MenuButton from "./MenuButton"
 import MenuDropdown from "./MenuDropdown"
 import MenuItem from "./MenuItem"
@@ -7,15 +7,29 @@ type Props = {
     children : React.ReactNode
 }
 
+const MenuContext = createContext({})
+
 export default function Menu({children}: Props): React.ReactElement {
+    const [open, setOpen] = useState(false)
+
+    function openMenu(value: boolean) : void {
+        setOpen(value)
+    }
     
     return (
-        <div className="relative">
-            {children}
-        </div>
+        <MenuContext.Provider value={{
+            open,
+            openMenu
+        }}>
+            <div className="relative">
+                {children}
+            </div>
+        </MenuContext.Provider>
     )
 }
 
 Menu.Button = MenuButton
 Menu.Dropdown = MenuDropdown
 Menu.Item = MenuItem
+
+export { MenuContext }
